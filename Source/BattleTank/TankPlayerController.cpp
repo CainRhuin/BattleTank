4b93e382afd9e188c066042361c3fa8e74b9b554
Mike Hill <bushballmarine@gmail.com>
2021-04-28 09:32:54 -0700

    set up functions to get ready for linetrace
    incomplete, outputs a false location to test that the GetSightRayHitLocation function works so far

--- Source/BattleTank/TankPlayerController.cpp
+++ Source/BattleTank/TankPlayerController.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+// #include "Engine/World.h"
 #include "TankPlayerController.h"
 
 void ATankPlayerController::BeginPlay()
@@ -33,7 +33,20 @@ void ATankPlayerController::AimTowardsCrosshair()
 {
     if (!GetControlledTank()){return;}
 
-    // get world location linetrace through crosshair
-    // if it hits something
-        // tell controlled tank to aim at this point
+    FVector HitLocation; // Out Param
+
+    if (GetSightRayHitLocation(HitLocation))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+        GetSightRayHitLocation(HitLocation);
+
+        // TODO tell controlled tank to aim at this point
+    }
+}
+
+// Get world location of linetrace through crosshair, true if it hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+    OutHitLocation = FVector(1.0);
+    return true;
 }
